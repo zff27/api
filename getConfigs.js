4354046b593063716347082bca971b387d3ebd83
zff27 <51163070+zff27@users.noreply.github.com>
2019-05-29 20:51:45 +0800

    Create getConfigs.js

---
+++ getConfigs.js
@@ -0,0 +1,11 @@
+
+{
+    "host" : "http://pay.zhongfeipay.com/",
+    "version" : "1.6.8.0529_R",
+    "about" : "https://github.com/huyquan/pmt",
+    "tryNotifyInterval" : 5000,
+    "tryCreateQRCodeInterval" : 5000,
+    "pushOnlineInterval" : 5000,
+    "remarkLength" : 15,
+    "remarkPrefix" : "KDF"
+}
