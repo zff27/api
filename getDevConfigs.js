6349f6271acd14034c9bf502709ccb05da1e9989
zff27 <51163070+zff27@users.noreply.github.com>
2019-06-10 22:11:20 +0800

    Create getDevConfigs.js

---
+++ getDevConfigs.js
@@ -0,0 +1,10 @@
+{
+    "host" : "http://dev-xpay.proxy.koudaiplus.com/xpay/",
+    "version" : "1.6.8.0610_RC",
+    "about" : "https://github.com/huyquan/pmt",
+    "tryNotifyInterval" : 5000,
+    "tryCreateQRCodeInterval" : 5000,
+    "pushOnlineInterval" : 5000,
+    "remarkLength" : 15,
+    "remarkPrefix" : "KDF"
+}
