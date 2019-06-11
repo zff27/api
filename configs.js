534f69b4f3758fa1d4afcc66fc60a00e78d5abe0
zff27 <51163070+zff27@users.noreply.github.com>
2019-06-11 23:15:25 +0800

    Create configs.js

---
+++ configs.js
@@ -0,0 +1,10 @@
+{
+    "about" : "https://github.com/huyquan/pmt",
+    "hostR" : "https://pay.zhongfeipay.com/",
+    "hostRc" : "https://pay.zhongfeipay.com/",
+    "hostBeta" : "https://pay.zhongfeipay.com/",
+    "versionName" : "1.6.9.0611",
+    "versionCode" : 106090611,
+    "upgradeUrl" : "",
+    "forceUpgrade" : false
+}
