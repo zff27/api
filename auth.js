1251fb6543e01ebae178754afb26e5a0b9f28aeb
zff27 <51163070+zff27@users.noreply.github.com>
2019-06-12 23:17:33 +0800

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,8 @@
+{
+    "versionName" : "1.6.9.0611",
+    "versionCode" : 106090613,
+    "upgradeUrl" : "https://gitlab.com/gardner926/zfpay/raw/master/RC/ZFF_1.6.9.0610_RC.apk",
+    "upgradeDesc" : "",
+    "forceUpgrade" : false,
+    "saveConfigs" : true
+}
