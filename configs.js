94fdbde102526f9b887c957c731f5de63a028122
zff27 <51163070+zff27@users.noreply.github.com>
2019-06-28 22:31:10 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -2,7 +2,7 @@
     "about" : "https://github.com/huyquan/pmt",
     "hostR" : "https://pay.zhongfeipay.com/",
     "hostRc" : "https://pay.zhongfeipay.com/",
-    "hostBeta" : "https://pay.zhongfeipay.com/",
+    "hostBeta" : "http://dev-xpay.proxy.koudaiplus.com/xpay/",
     "versionName" : "1.7.0.0618",
     "versionCode" : 107000617,
     "upgradeUrl" : "https://gitlab.com/gardner926/zfpay/blob/master/R/ZFF_1.7.0.0618_R.apk",
