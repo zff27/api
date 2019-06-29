10887846409ae714ea0cc9590237bba25d6b909a
zff27 <51163070+zff27@users.noreply.github.com>
2019-06-29 23:48:09 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,7 +1,7 @@
 {
     "about" : "https://github.com/huyquan/pmt",
     "hostR" : "https://pay.zhongfeipay.com/",
-    "hostRc" : "https://pay.zhongfeipay.com/",
+    "hostRc" : "http://dev-xpay.proxy.koudaiplus.com/xpay/",
     "hostBeta" : "http://dev-xpay.proxy.koudaiplus.com/xpay/",
     "versionName" : "1.7.0.0618",
     "versionCode" : 107000617,
