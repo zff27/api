2161087fcd1e94ec733a9c7e70ef87a7a6a51d5c
zff27 <51163070+zff27@users.noreply.github.com>
2019-07-09 22:32:39 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -5,7 +5,7 @@
     "hostBeta" : "http://dev-xpay.proxy.koudaiplus.com/xpay/",
     "versionName" : "1.7.0.0618",
     "versionCode" : 107000617,
-    "upgradeUrl" : "https://gitlab.com/gardner926/zfpay/blob/master/R/ZFF_1.7.0.0618_R.apk",
+    "upgradeUrl" : "",
     "upgradeDesc" : "",
     "forceUpgrade" : false
 }
