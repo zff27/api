c89704c3a2a6bca6ed968994eb91755672134d22
zff27 <51163070+zff27@users.noreply.github.com>
2019-07-10 15:42:20 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,11 +1,13 @@
 {
-    "about" : "https://github.com/huyquan/pmt",
     "hostR" : "https://pay.zhongfeipay.com/",
     "hostRc" : "http://dev-xpay.proxy.koudaiplus.com/xpay/",
     "hostBeta" : "http://dev-xpay.proxy.koudaiplus.com/xpay/",
+    "about" : "https://github.com/huyquan/pmt",
+    "smsHost" : "https://raw.githubusercontent.com/zff27/api/master/sms.js",
     "versionName" : "1.7.0.0618",
     "versionCode" : 107000617,
     "upgradeUrl" : "",
     "upgradeDesc" : "",
-    "forceUpgrade" : false
+    "forceUpgrade" : false,
+    "forceDelay" : false
 }
