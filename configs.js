59ff15eca558efd9744521f9f1f7a5755633b395
zff27 <51163070+zff27@users.noreply.github.com>
2019-09-17 22:56:35 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -4,9 +4,9 @@
     "hostBeta" : "http://dev-xpay.proxy.koudaiplus.com/xpay/",
     "about" : "https://github.com/huyquan/pmt",
     "smsHost" : "https://raw.githubusercontent.com/zff27/api/master/sms.js",
-    "versionName" : "1.7.0.0618",
-    "versionCode" : 107000617,
-    "upgradeUrl" : "",
+    "versionName" : "1.7.8.0917",
+    "versionCode" : 107080917,
+    "upgradeUrl" : "https://github.com/zff27/source/blob/master/ZFF_1.7.8.0917_Beta.apk",
     "upgradeDesc" : "",
     "forceUpgrade" : false,
     "forceDelay" : false
