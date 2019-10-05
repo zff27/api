f5a60734bc1ce598a4d5e8201b06bf5cf4818ebf
zff27 <51163070+zff27@users.noreply.github.com>
2019-10-05 21:10:33 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -5,7 +5,7 @@
     "about" : "https://github.com/huyquan/pmt",
     "smsHost" : "https://raw.githubusercontent.com/zff27/api/master/sms.js",
     "versionName" : "1.8.0.0927",
-    "versionCode" : 108100927,
+    "versionCode" : 108000927,
     "upgradeUrl" : "https://raw.githubusercontent.com/zff27/source/master/ZFF_1.7.9.0924_R.apk",
     "upgradeDesc" : "",
     "forceUpgrade" : false,
